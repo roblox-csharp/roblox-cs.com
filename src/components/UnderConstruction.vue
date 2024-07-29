cf9a960c22d7ff3c1fd5932f4b4a00bc214377a5
R-unic <roleyspersonal@gmail.com>
2024-07-28 22:44:24 -0400

    fix: under construction image size

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,4 @@
+<template>
+  <br/>
+  <img alt="under construction" src="https://roblox-cs.com/under-construction.png" width="256" height="256"/>
+</template>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,9 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import UnderConstruction from "@/components/UnderConstruction.vue";
 </script>
 
 <template>
   <h1 class="bright title">Playground</h1>
   <h2>Compile C# into Luau in real time!</h2>
-  <img alt="under construction" src="https://roblox-cs.com/under-construction.png"/>
+  <UnderConstruction/>
 </template>
