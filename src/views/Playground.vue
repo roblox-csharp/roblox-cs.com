df0c6c097bf2cf8fceaea9378f3760324c161670
aqzp <72946059+AsynchronousAI@users.noreply.github.com>
2024-09-28 10:32:06 -0500

    add playground to website

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,10 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-import UnderConstruction from "@/components/UnderConstruction.vue";
-</script>
-
 <template>
   <h1 class="bright title">Playground</h1>
   <h2>Compile C# into Luau in real time!</h2>
-  <UnderConstruction/>
+  <iframe 
+    src="https://asynchronousai-roblox-cs.hf.space/?logs=build&amp;__theme=system"
+    width="100%"
+    height="100%"
+    allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; clipboard-read; clipboard-write; display-capture; document-domain; encrypted-media; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; vr ; wake-lock; xr-spatial-tracking"
+    sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-storage-access-by-user-activation"
+  >
+  </iframe>
 </template>
